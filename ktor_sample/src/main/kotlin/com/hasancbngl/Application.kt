8449675bc6fc87149831cdffdc306c4466441988
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2023-03-10 02:40:01 +0300

    commit changes

--- ktor_sample/src/main/kotlin/com/hasancbngl/Application.kt
+++ ktor_sample/src/main/kotlin/com/hasancbngl/Application.kt
@@ -4,10 +4,13 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+//specify server engine
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
 
 fun Application.module() {
     routing {
+        //declare routing so our server should response to get request
         get("/") {
             call.respondText("Is it working?\n\nyees")
         }
