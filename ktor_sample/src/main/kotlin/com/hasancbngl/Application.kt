110a18c7e36e7a7e43d7f700beeef8a7317cd896
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2023-06-05 14:30:11 -0500

    commit changes

--- ktor_sample/src/main/kotlin/com/hasancbngl/Application.kt
+++ ktor_sample/src/main/kotlin/com/hasancbngl/Application.kt
@@ -1,5 +1,6 @@
 package com.hasancbngl
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
@@ -17,12 +18,34 @@ fun Application.module() {
         get("/") {
             call.respondText("Is it working?\n\nyees")
         }
+        get("/users/{username}") {
+            val username = call.parameters["username"]
+            val header = call.request.headers["connection"]
+
+            if (username=="hasan") {
+                call.response.header("customHeader" , "is it working?")
+                call.respond(message = "Hello, $username, u doing good", status = HttpStatusCode.OK)
+            } else call.respondText("user not found")
+        }
+        get("/user") {
+            call.respondText("user page", status = HttpStatusCode.OK)
+        }
+
+     /*
+      get request and basically same thing as above
+      route(path = "/route", method = HttpMethod.Get){
+            handle { call.respondText("longer function using route") }
+        } */
+        post ("/p"){
+            call.respondText("item posted")
+        }
     }
 }
 
 @Suppress("unused")
 fun Application.module2() {
     //install call logging plugin
+    //plugin allows to show certain logging in ktor project
     install(CallLogging)
     routing { //routing plugin
         //declare routing so our server should response to get request
