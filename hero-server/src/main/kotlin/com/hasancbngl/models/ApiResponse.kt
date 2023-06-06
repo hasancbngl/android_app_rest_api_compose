396f04e3743dcb92c0257c8b5ff4a23d3ace1a03
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2023-06-06 13:32:17 -0500

    implement model and pi response classes

---
+++ hero-server/src/main/kotlin/com/hasancbngl/models/ApiResponse.kt
@@ -0,0 +1,9 @@
+package com.hasancbngl.models
+
+data class ApiResponse(
+    val success: Boolean,
+    val message: String? = null,
+    val prevPage: Int? = null,
+    val nextPage: Int? = null,
+    val heroes: List<Hero> = emptyList()
+)
