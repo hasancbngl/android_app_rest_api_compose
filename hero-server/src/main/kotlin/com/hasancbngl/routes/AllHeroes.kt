2da2ccb5d1a8ddbcdec89170abe068e2bb1e056f
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2023-06-06 20:00:29 -0500

    complete implementation for getAllHeroes endpoint

--- hero-server/src/main/kotlin/com/hasancbngl/routes/AllHeroes.kt
+++ hero-server/src/main/kotlin/com/hasancbngl/routes/AllHeroes.kt
@@ -1,19 +1,24 @@
 package com.hasancbngl.routes
 
 import com.hasancbngl.models.ApiResponse
+import com.hasancbngl.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.NumberFormatException
 
 fun Route.getAllHeroes() {
+    val heroRepository : HeroRepository by inject()
+
     get("/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             //require the page only be within 1-5 otherwise throw an exception
             require(page in 1..5)
-            call.respond(message = page)
+            val apiResponse = heroRepository.getAllHeroes(page)
+            call.respond(message = apiResponse, status = HttpStatusCode.OK)
         } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(
