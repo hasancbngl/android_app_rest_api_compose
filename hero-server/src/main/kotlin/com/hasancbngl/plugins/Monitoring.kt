2c6b7eebbdd57a54ecfd6b928212ac4272d5ade7
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2023-08-28 13:59:12 -0500

    commit changes

--- hero-server/src/main/kotlin/com/hasancbngl/plugins/Monitoring.kt
+++
@@ -1,8 +0,0 @@
-package com.hasancbngl.plugins
-
-import io.ktor.server.application.*
-import io.ktor.server.plugins.callloging.*
-
-fun Application.configureMonitoring() {
-    install(CallLogging)
-}
